5c00edd0107ab741a8168c833b506fdfb3367215
mhx2n <mhdiv2331@gmail.com>
2026-05-06 01:13:26 +0600

    Update index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,3 +1,4 @@
+import health from "./health";
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
 import quizzesRouter from "./quizzes";
@@ -8,5 +9,5 @@ const router: IRouter = Router();
 router.use(healthRouter);
 router.use(quizzesRouter);
 router.use(telegramRouter);
-
+router.use("/health", health);
 export default router;
